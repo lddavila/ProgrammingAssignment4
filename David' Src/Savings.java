1b319b1cc7517d93e22f8baa3cb74d1e57cbaa40
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2021-03-30 18:28:22 -0600

    Update Savings.java

--- David' Src/Savings.java
+++ David' Src/Savings.java
@@ -12,14 +12,7 @@ public class Savings extends Account{
      * Assumes that I would always have perfectly formatted data
      */
     public Savings(String[] bankAccountData) {
-        this.set_First_Name(bankAccountData[0]);
-        this.set_Last_Name(bankAccountData[1]);
-        //converts the string into the expected long
-        this.set_Account_Number(Long.parseLong(bankAccountData[10]));
-        //converts string into expected boolean
 
-        //converts string into expected double
-        this.set_Starting_Balance(Double.parseDouble(bankAccountData[13]));
     }
 
 
