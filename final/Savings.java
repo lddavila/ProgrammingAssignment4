a2bc019302d7b87d519189f343d5a7ef823d75d8
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2021-03-30 18:37:06 -0600

    Create Savings.java

---
+++ final/Savings.java
@@ -0,0 +1,19 @@
+/** extension of the Account Class, with additional attributes
+ *
+ */
+public class Savings extends Account{
+    /**default constructor
+     */
+    public Savings(){
+
+    }
+    /**Constructor which takes a String array and takes info from it according to array position
+     * @param bankAccountData
+     * Assumes that I would always have perfectly formatted data
+     */
+    public Savings(String[] bankAccountData) {
+
+    }
+
+
+}
