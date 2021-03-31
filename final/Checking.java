b3189baede0e79b3a016febac5e96f20ae7b3916
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2021-03-30 18:32:59 -0600

    Create Checking.java

---
+++ final/Checking.java
@@ -0,0 +1,18 @@
+/** extension of the Account Class, with additional attributes
+ *
+ */
+public class Checking extends Account{
+
+    /**default constructor
+     */
+    public Checking(){}
+
+    /**Constructor which takes a String array and takes info from it according to array position
+     * @param bankAccountData
+     * Assumes that I would always have perfectly formatted data
+     */
+    public Checking(String[] bankAccountData) {
+    }
+
+
+}
