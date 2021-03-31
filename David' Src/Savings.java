1e44bdd5fcd7d363980ebec1211f367540c5cd1c
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2021-03-30 19:07:45 -0600

    Update Savings.java

--- David' Src/Savings.java
+++ David' Src/Savings.java
@@ -14,6 +14,16 @@ public class Savings extends Account{
     public Savings(String[] bankAccountData) {
 
     }
-
+    
+    
+    public Savings(String [] bankAccountData, int [] dataPositions){
+        Savings tempSavings = new Savings();
+        tempSavings.set_First_Name(bankAccountData[dataPositions[14]]);
+        tempSavings.set_Last_Name(bankAccountData[dataPositions[2]]);
+        tempSavings.set_Starting_Balance(Double.parseDouble(bankAccountData[dataPositions[8]]));
+        tempSavings.set_Account_Number(Long.parseLong(bankAccountData[dataPositions[0]]));
+        tempSavings.getBankStatement().setStartingBalance(Double.parseDouble(bankAccountData[dataPositions[8]]));
+        return tempSavings;
+    }
 
 }
