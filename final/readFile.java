65d85de501d2a48a3b7c8b2fc1e71cd7f7b725db
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2021-04-06 13:08:20 -0600

    Update readFile.java

--- final/readFile.java
+++ final/readFile.java
@@ -30,38 +30,45 @@ public class readFile {
      */
     public static int [] findColumnPosition(String [] columnInfo){
 
-        /*13
+                /*13
         //columnPosition[0] = Savings Account number
         //columnPosition[1] = ID number
         //columnPosition[2] =  Last Name
         //columnPosition[3] = DOB
-        //columnPosition[4] = Checking Account Number
-        //columnPosition[5] = Credit Account Number
-        //columnPosition[6] = Phone Number
-        //columnPosition[7] = Checking Starting Balance
-        //columnPosition[8] = Saving Starting Balance
-        //columnPosition[9] = Credit Max
-        //columnPosition[10] = Credit Starting Balance
-        //columnPosition[11] = Address
-        //columnPosition[12] = Address
-        //ColumnPosition[13] = Address
-        //ColumnPosition[14] = first name
+        //columnPosition[4] = DOB
+        //columnPosition[5] = Checking Account Number
+        //columnPosition[6] = Credit Account Number
+        //columnPosition[7] = Phone Number
+        //columnPosition[8] = Checking Starting Balance
+        //columnPosition[9] = Saving Starting Balance
+        //columnPosition[10] = Credit Max
+        //columnPosition[11] = Credit Starting Balance
+        //ColumnPosition[12] = first name
+        //ColumnPosition[13] = Email
+        //ColumnPosition[14] = password
+        //columnPosition[15] = Address
+        //columnPosition[16] = Address
+        //ColumnPosition[17] = Address
 
          */
 
 
-        int [] columnPosition = new int[columnInfo.length+2];
+        int [] columnPosition = new int[columnInfo.length+3];
         for(int i = 0; i < columnPosition.length;i++){
             columnPosition[i] = -111111;
         }
 
+        System.out.println("///////////////////////////");
+        System.out.println(columnPosition.length);
+        System.out.println("///////////////////////////");
+        int counter = 0;
 
-        for (int i = 0; i < columnInfo.length; i++ ){
+        for (int i = 0; i < columnPosition.length; i++ ){
             //I use this switch case to find which column each piece of info is stored in
             //I then store those positions into an array which I then use to make the appropriate calls to build
             //the databases
 
-            switch(columnInfo[i]){
+            switch(columnInfo[counter]){
                 case "Savings Account Number":
                     columnPosition[0] = i;
                     break;
@@ -73,47 +80,73 @@ public class readFile {
                     break;
                 case "Date of Birth":
                     columnPosition[3] = i;
+                    columnPosition[4] = i+1;
+                    i+=1;
                     break;
                 case "Checking Account Number":
-                    columnPosition[4] = i;
-                    break;
-                case "Credit Account Number":
                     columnPosition[5] = i;
                     break;
-                case "Phone Number":
+                case "Credit Account Number":
                     columnPosition[6] = i;
                     break;
-                case "Checking Starting Balance":
+                case "Phone Number":
                     columnPosition[7] = i;
                     break;
-                case "Savings Starting Balance":
+                case "Checking Starting Balance":
                     columnPosition[8] = i;
                     break;
-                case "Credit Max":
+                case "Savings Starting Balance":
                     columnPosition[9] = i;
                     break;
-                case "Credit Starting Balance":
+                case "Credit Max":
                     columnPosition[10] = i;
                     break;
-                case "Address":
+                case "Credit Starting Balance":
                     columnPosition[11] = i;
-                    columnPosition[12] = i+1;
-                    columnPosition[13] = i+2;
-                    //we do this here because the address actually takes up 3 columns cause of how
-                    //it is formatted in the csv
                     break;
                 case "First Name":
-                    columnPosition[14] = i+2;
+                    columnPosition[12] = i;
                     break;
+                case "Email":
+                    columnPosition[13] = i;
+                    break;
+                case "Password":
+                    columnPosition[14] = i;
+                    break;
+                case "Address":
+                    columnPosition[15] = i;
+                    columnPosition[16] = i+1;
+                    columnPosition[17] = i+2;
+                    i+=2;
+                    //we do this here because the address actually takes up 3 columns cause of how
+                    //it is formatted in the csv
+                    break; }
+                    counter++;
 
-
-
-            }
 
         }
-        for(int i = 0; i < columnPosition.length; i++){
 
-        }
+        /*
+        System.out.println("Savings Account Number: " + columnPosition[0]);
+        System.out.println("ID Number: " + columnPosition[1]);
+        System.out.println("Last NAme: " + columnPosition[2]);
+        System.out.println("Date Of Birth: " + columnPosition[3]);
+        System.out.println("Date Of Birth: " + columnPosition[4]);
+        System.out.println("Checking Account Number: " + columnPosition[5]);
+        System.out.println("Credit Account Number: " + columnPosition[6]);
+        System.out.println("Phone Number: " + columnPosition[7]);
+        System.out.println("Checking Starting Balance: " + columnPosition[8]);
+        System.out.println("Saving Starting Balance: " + columnPosition[9]);
+        System.out.println("Credit Max: " + columnPosition[10]);
+        System.out.println("Credit Starting Balance: " + columnPosition[11]);
+        System.out.println("first name: " + columnPosition[12]);
+        System.out.println("Email: " + columnPosition[13]);
+        System.out.println("Password: " + columnPosition[14]);
+        System.out.println("Address: " + columnPosition[15]);
+        System.out.println("Address: " + columnPosition[16]);
+        System.out.println("Address: " + columnPosition[17]);
+
+         */
         return columnPosition;
     }
 
@@ -143,6 +176,13 @@ public class readFile {
 
                 //Takes the String and turns it to an array to take advantage of a constructor in the Checkings class which accepts an array
                 String [] bankAccountData = reader.nextLine().split(",");
+                //the loop below lets us print the data, so we can see exactly how it's formatted in future lab iterations, don't delete
+                /*
+                for (int i = 0; i < bankAccountData.length; i++){
+                    System.out.println(i + ": "+ bankAccountData[i]);
+                }
+                 */
+
                 //creates a temp customer to add to database
                 Customer tempCust = new Customer(bankAccountData, columnPositions);
                 //adds temporary customer to array list to preserve it
